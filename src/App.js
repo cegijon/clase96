914a00f66fcb3ff0221b497329cd50ced75d1a73
cegijon <53779413+cegijon@users.noreply.github.com>
2020-04-15 17:35:27 +1000

    busqueda y uso de useEffect

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-
   const [pokemon, setPokemon] = useState({})
+  const [busqueda, setBusqueda] = useState('pikachu')
 
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+  const buscarPokemon = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${busqueda}`)
       .then(res => res.json())
-      .then(data => setPokemon(data))
-  }, [])
+      .then(data => setPokemon(data));
+  }
+
+  const handleChange = e => {
+    setBusqueda(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    buscarPokemon()
+  }
+
+  useEffect(buscarPokemon, [])
 
   console.log(pokemon)
 
@@ -24,6 +35,10 @@ const App = () => {
           </>
         }
       </div>
+      <form onSubmit={handleSubmit}>
+      <input value={busqueda} onChange={handleChange}></input>
+      <input type="submit" value="Buscar pokemon"></input>
+      </form>
     </div>
   );
 }
